042c647d459d588b3432357228abce0aa8de3de3
Ivo Bubeňko <ivobubenko@Ivos-MacBook-Air-2.local>
2026-02-26 13:22:23 +0100

    adjusted variables description

--- section-1/variables.tf
+++ section-1/variables.tf
@@ -1,11 +1,11 @@
 variable "private_cidr_blocks" {
-  description = "Ip range for the given subnet"
+  description = "cidr_blocks for the private subnets"
   default     = { database : "10.0.1.0/24", server : "10.0.2.0/24" }
   type        = map(string)
 }
 
 variable "public_cidr_block" {
-  description = "Ip range for the given subnet"
+  description = "cidr_block for the public subnet"
   default     = "10.0.3.0/24"
   type        = string
 }
